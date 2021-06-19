9be6596c5ad32f67c2860d3a3b745a8b78e7ecd8
Jaeger2305 <rwebb2305@gmail.com>
2021-06-19 20:26:42 +0200

    feat: add LERP and scalign script for buttons

---
+++ Assets/GenericButton.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems; // Required when using Event data.
+
+public class GenericButton : MonoBehaviour, IPointerDownHandler
+{
+    public float clickScale = 0.75f;
+
+    public float timeToLerp = 0.2f;
+    float targetScale = 1f;
+    float scaleModifier;
+
+    IEnumerator LerpFunction(float endValue, float duration)
+    {
+        float time = 0;
+        float startValue = scaleModifier;
+        Vector3 startScale = transform.localScale;
+
+        while (time < duration)
+        {
+            scaleModifier = Mathf.Lerp(startValue, endValue, time / duration);
+            transform.localScale = startScale * scaleModifier;
+            time += Time.deltaTime;
+            yield return null;
+        }
+        transform.localScale = startScale * targetScale;
+        scaleModifier = targetScale;
+    }
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        transform.localScale = new Vector3(clickScale, clickScale, clickScale);
+        scaleModifier = clickScale;
+        StartCoroutine(LerpFunction(targetScale, timeToLerp));
+    }
+}
