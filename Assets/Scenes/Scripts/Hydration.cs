10fe33b796c35d7ab9f3a7188ad5efa2b0930633
MrInformatic <maxundmoritz99199@gmail.com>
2021-06-19 16:15:02 +0200

    feat: add hydration slider

---
+++ Assets/Scenes/Scripts/Hydration.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Hydration : MonoBehaviour
+{
+    public Slider HydraionSlider;
+    
+    void Start()
+    {
+        HydraionSlider = GetComponent<Slider>();
+    }
+
+    void Update()
+    {
+        
+    }
+}
